b86920e7cf301d5ad9f8adbd8a4b014a39c99e87
avirahul007 <63554722+avirahul007@users.noreply.github.com>
2021-02-14 16:43:01 +0400

    Delete modules/cloudfront directory

--- modules/cloudfront/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "waf_web_acl_id" {
-  value = "${aws_waf_web_acl.WAFWebACL.id}"
-}
--- modules/cloudfront/waf_acl.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_waf_web_acl" "WAFWebACL" {
-  name        = "${local.waf_web_acl}"
-  metric_name = "SecurityAutomationsMaliciousRequesters"
-
-  default_action {
-    type = "ALLOW"
-  }
-}
